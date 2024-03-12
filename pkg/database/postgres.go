c55f61d4fa1d11d73435b9aa12e30b7a264c73f3
Goberman15 <akbarrmdhn94@gmail.com>
2024-03-13 06:57:04 +0700

    Remove Dupliated Port on PG Connection

--- pkg/database/postgres.go
+++ pkg/database/postgres.go
@@ -3,18 +3,18 @@ package database
 import (
 	"fmt"
 
+	_ "github.com/jackc/pgx/v4/stdlib"
 	"github.com/jmoiron/sqlx"
 	"github.com/ramadhan1445sprint/sprint_ecommerce/config"
 )
 
 func NewDatabase() (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		config.GetString("DB_HOST"),
 		config.GetString("DB_PORT"),
 		config.GetString("DB_USERNAME"),
 		config.GetString("DB_PASSWORD"),
 		config.GetString("DB_NAME"),
-		config.GetString("DB_PORT"),
 	)
 
 	db, err := sqlx.Connect("pgx", dsn)
