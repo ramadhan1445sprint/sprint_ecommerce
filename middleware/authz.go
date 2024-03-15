3b52370e972da254a45500b159f9ac716e757a14
Goberman15 <akbarrmdhn94@gmail.com>
2024-03-16 02:20:29 +0700

    Create Auth Specific for Product Page

--- middleware/authz.go
+++ middleware/authz.go
@@ -35,3 +35,32 @@ func Authorization(ctx *fiber.Ctx) error {
 	ctx.Locals("name", payload.Name)
 	return ctx.Next()
 }
+
+func ProductPageAuth(ctx *fiber.Ctx) error {
+	userOnly := ctx.Query("userOnly")
+
+	if userOnly == "true" {
+		auth := ctx.Get("Authorization")
+		if auth == "" {
+			return customErr.NewUnauthorizedError("token not found")
+		}
+	
+		splitted := strings.Split(auth, " ")
+	
+		if splitted[0] != "Bearer" {
+			return customErr.NewUnauthorizedError("invalid token")
+		}
+	
+		payload, err := crypto.VerifyToken(splitted[1])
+		if err != nil {
+			if errors.Is(err, jwt.ErrTokenExpired) {
+				return customErr.NewUnauthorizedError("token expired")
+			}
+			return customErr.NewUnauthorizedError(err.Error())
+		}
+	
+		ctx.Locals("user_id", payload.Id)
+	}
+
+	return ctx.Next()
+}
