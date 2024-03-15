38f36c340d48fc38eebcf8a9801ae80f97b91557
Goberman15 <akbarrmdhn94@gmail.com>
2024-03-15 17:09:53 +0700

    Add Prometheus Instrumentation

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/jackc/pgx/v4 v4.18.3
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/joho/godotenv v1.5.1
+	github.com/prometheus/client_golang v1.19.0
 	golang.org/x/crypto v0.21.0
 )
 
@@ -32,6 +33,8 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.28.4 // indirect
 	github.com/aws/smithy-go v1.20.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
 	github.com/jackc/pgconn v1.14.3 // indirect
 	github.com/jackc/pgio v1.0.0 // indirect
@@ -43,10 +46,14 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.32.0 // indirect
 )
--- server/server.go
+++ server/server.go
@@ -2,14 +2,23 @@ package server
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"strconv"
+	"time"
 
 	awsCfg "github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/jmoiron/sqlx"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/ramadhan1445sprint/sprint_ecommerce/config"
 	"github.com/ramadhan1445sprint/sprint_ecommerce/controller"
+	"github.com/ramadhan1445sprint/sprint_ecommerce/customErr"
 	"github.com/ramadhan1445sprint/sprint_ecommerce/middleware"
 	"github.com/ramadhan1445sprint/sprint_ecommerce/repo"
 	"github.com/ramadhan1445sprint/sprint_ecommerce/svc"
@@ -20,9 +29,51 @@ type Server struct {
 	app *fiber.App
 }
 
+// var (
+// 	reqDurationHist = promauto.NewHistogramVec(prometheus.HistogramOpts{
+// 		Name:    "shopifyx_http_request_duration_seconds",
+// 		Help:    "Duration of HTTP requests.",
+// 		Buckets: prometheus.LinearBuckets(1, 1, 10),
+// 	}, []string{"path", "method", "status"})
+// )
+
 func NewServer(db *sqlx.DB) *Server {
+	reqDurationHist := promauto.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "shopifyx_http_request_duration_seconds",
+		Help:    "Duration of HTTP requests.",
+		Buckets: prometheus.LinearBuckets(1, 1, 10),
+	}, []string{"path", "method", "status"})
+
 	app := fiber.New(fiber.Config{
 		ErrorHandler: middleware.ErrorHandler,
+	})
+
+	app.Use(recover.New())
+	app.Get("/metrics", adaptor.HTTPHandler(promhttp.Handler()))
+
+	app.Use(func(ctx *fiber.Ctx) error {
+		start := time.Now()
+		method := ctx.Method()
+		path := ctx.Path()
+
+		err := ctx.Next()
+
+		statusCode := strconv.Itoa(ctx.Response().StatusCode())
+
+		if err != nil {
+			if customError, ok := err.(customErr.CustomError); ok {
+				statusCode = strconv.Itoa(customError.Status())
+			} else {
+				statusCode = "500"
+			}
+		}
+
+		elapsedDuration := time.Since(start).Seconds()
+
+		fmt.Println(method, path, statusCode, elapsedDuration)
+		reqDurationHist.WithLabelValues(path, method, statusCode).Observe(elapsedDuration)
+
+		return err
 	})
 
 	return &Server{
